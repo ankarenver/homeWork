6db353e8d5772ba9c1b1b736c8e79e09f1eeccc6
ankarenver <1996ankarjian@gmail.com>
2020-03-06 16:59:34 -0500

    homeWork third push

--- src/main/java/com/cbt/tests/NavigationTests.java
+++ src/main/java/com/cbt/tests/NavigationTests.java
@@ -20,7 +20,7 @@ public class NavigationTests {
          */
 
 //        testForChrome();
-        testForFirefox();
+//        testForFirefox();
 //        testForEdge();
 
 
--- src/main/java/com/cbt/utilities/TitleVerification.java
+++ src/main/java/com/cbt/utilities/TitleVerification.java
@@ -18,7 +18,7 @@ public class TitleVerification {
         for (String each : urls){
             driver.get(each);
             titles.add(driver.getTitle());
-            System.out.println(each.startsWith("http://practice.cybertekschool.com"));
+            System.out.println(driver.getCurrentUrl().startsWith("http://practice.cybertekschool.com"));
             driver.close();
 
         }
