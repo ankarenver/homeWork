3ffee75bc1847b3e44490e3fc930c8f1df893d38
ankarenver <1996ankarjian@gmail.com>
2020-03-06 19:59:01 -0500

    homeWork third push

---
+++ src/main/java/com/cbt/utilities/TitleVerification2.java
@@ -0,0 +1,36 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TitleVerification2 {
+    public static void main(String[] args) {
+
+        List<String> urls = Arrays.asList("https://www.luluandgeorgia.com/", "https://wayfair.com/", "https://walmart.com", "https://westelm.com/");
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+
+        for (String each : urls){
+            driver.get(each);
+            String title = driver.getTitle().replace(" ","").toLowerCase();
+            for (int i = 0; i <title.length()-2 ; i++) {
+                if(title.substring(i,i+3).equals("com")){
+                    title=title.substring(0,i);
+                }
+            }
+//            System.out.println(title);
+            String url = driver.getCurrentUrl();
+//            System.out.println(url);
+            System.out.println(url.contains(title));
+
+        }
+        driver.close();
+
+
+
+
+
+    }
+}
